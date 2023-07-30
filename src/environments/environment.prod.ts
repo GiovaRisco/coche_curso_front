adbd3de7fa827972478e0a9fdee3936c29880813
GiovaRisco <grisco141@gmail.com>
2023-07-30 18:25:15 -0500

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://cochescurso-production.up.railway.app/api"
+  apiUrl: "https://cochescurso-production.up.railway.app/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://cochescurso-production.up.railway.app/api"
+  apiUrl: "https://cochescurso-production.up.railway.app/api"
 };
 
 /*
